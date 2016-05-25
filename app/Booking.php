b8f1ee93adfa4e550077575a708540bc8433f636
damian <damian@omnimedia.co.nz>
2016-05-25 13:19:04 +1200

    mod booking model

--- app/Booking.php
+++ app/Booking.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Booking extends Model
 {
-    protected $table = 'bookings'; // you may change this to your name table
-public $timestamps = true; // set true if you are using created_at and updated_at
-// protected $primaryKey = 'my_id'; // the default is id
+	protected $table = 'bookings'; // you may change this to your name table
+	public $timestamps = true; // set true if you are using created_at and updated_at
+	// protected $primaryKey = 'my_id'; // the default is id
 }
